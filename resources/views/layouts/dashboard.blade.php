a21be3966a786f0791c1030303f6fc6c3fe82d58
Leonardo Maldonado <lrmaldo@gmail.com>
2023-09-08 09:14:46 -0600

    agregar campo de celular en el dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,11 +6,11 @@
 @php
     $total_encuestas = App\Models\encuesta::all()->count();
     $total_de_encuestados = App\Models\User::role('encuestado')->get();
-    
+
 
     /* variables de la grafica de genero */
-  $Masculino = App\Models\Datos_generales::where('genero','like','%Masculino%')->count(); 
-  $Femenino = App\Models\Datos_generales::where('genero','like','%Femenino%')->count(); 
+  $Masculino = App\Models\Datos_generales::where('genero','like','%Masculino%')->count();
+  $Femenino = App\Models\Datos_generales::where('genero','like','%Femenino%')->count();
 
   /* edades de 18 a 25 */
   $entre18 = App\Models\Datos_generales::whereBetween('edad',[18 , 25])->count();
@@ -20,8 +20,8 @@
   $entre31a35 = App\Models\Datos_generales::whereBetween('edad',[31 , 35])->count();
   /* más de 35 años */
   $masde36 = App\Models\Datos_generales::where('edad','>',36)->count();
- 
- 
+
+
         $total_edades=$entre18+$entre26a30+$entre31a35+$masde36;
         $array_edades =array($entre18,$entre26a30,$entre31a35,$masde36);
         $array_genero =array($Masculino,$Femenino);
@@ -30,7 +30,7 @@
 <div class="container">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        
+
       </div>
 <!-- Content Row -->
 <div class="row">
@@ -78,7 +78,7 @@
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-info text-uppercase mb-1"> <a class="text-info text-uppercase mb-1" href="{{ route('resultados') }}">Ir a Resultados</a> </div>
               <div class="row no-gutters align-items-center">
-                
+
               </div>
             </div>
             <div class="col-auto">
@@ -96,7 +96,7 @@
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div ><a class="text-xs font-weight-bold text-warning text-uppercase mb-1" href="{{ route('encuestas.index') }}">Ir a encuestas</a></div>
-             
+
             </div>
             <div class="col-auto">
               <i class="fas fa-file-alt fa-2x text-gray-300"></i>
@@ -120,7 +120,7 @@
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
           <h6 class="m-0 font-weight-bold text-primary">Gráfica de edades</h6>
-          
+
         </div>
         <!-- Card Body -->
         <div class="card-body">
@@ -136,16 +136,16 @@
       <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          
+
           <h6 class="m-0 font-weight-bold text-primary">   <i class="fas fa-chart-pie mr-1"></i>Gráfica de genero</h6>
-          
+
         </div>
         <!-- Card Body -->
         <div class="card-body">
           <div class="chart-pie ">
             <canvas id="migrafica_generos"></canvas>
           </div>
-          
+
         </div>
       </div>
     </div>
@@ -153,7 +153,7 @@
 
 
   <script>
-      
+
  Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
                             Chart.defaults.global.defaultFontColor = '#292b2c';
 
@@ -236,6 +236,7 @@
               <th>Ciudad</th>
               <th>CP</th>
               <th>Teléfono</th>
+              <th>Célular</th>
               <th >Fecha de registro</th>
           </thead>
           <tbody>
@@ -251,6 +252,7 @@
                   <td>{{ isset($user->Datos->ciudad)?$user->Datos->ciudad:'sin datos'}}</td>
                   <td>{{ isset($user->Datos->cp)?$user->Datos->cp:'sin datos'}}</td>
                   <td>{{ isset($user->Datos->telefono)?$user->Datos->telefono:'sin datos'}}</td>
+                  <td>{{ isset($user->Datos->celular)?$user->Datos->celular:'sin datos'}}</td>
                   <td>{{ isset($user->Datos->created_at)?date_format($user->Datos->created_at,'d/m/Y h:i:s A'):'sin datos' }}</td>
                   {{-- <td>
                       @if (!empty($user->getRoleNames()))
@@ -259,21 +261,21 @@
                           @endforeach
                       @endif
                   </td> --}}
-                
+
                      {{--  <a class="btn btn-info" href="{{ route('users.show', $user->id) }}">Ver</a>
                       <a class="btn btn-primary" href="{{ route('users.edit', $user->id) }}">Editar</a>
                       {!! Form::open(['method' => 'DELETE', 'route' => ['users.destroy', $user->id], 'style' =>
                       'display:inline']) !!}
                       {!! Form::submit('Eliminar', ['class' => 'btn btn-danger']) !!}
                       {!! Form::close() !!} --}}
-                
+
               </tr>
           @endforeach
           </tbody>
       </table>
-     
 
-  
+
+
   </div>
 </div>
 
